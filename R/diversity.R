e2d3054b3c4551c43e9c4450212a00182e6650d0
DuncanMcKinnon <duncanmckinnon96@gmail.com>
2020-07-18 10:01:57 -0700

    fixing bug in diversity helpers

--- R/diversity.R
+++ R/diversity.R
@@ -1,13 +1,13 @@
 #' Entropy Based Measures of Data Diversity
-#' @description Calculate the diversity of a dataset by determining the degree to which
-#' differentiable groupings in the columns vary from maximum entropy for the number of grouping.
+#' @description Calculate the diversity of a dataset or vector by determining the degree to which
+#' differentiable groups vary from maximum entropy for the number of unique grouping.
 #' If the groupings are uniformly distributed, the entropy is maximized.  This functions calculates
-#' the ratio of the real distribution of groupings to the maximum entropy distribution to determine
+#' the ratio of the entropy for the real distribution of groupings to the maximum entropy distribution to determine
 #' the overall diversity represented in the data.
 #' @importFrom dplyr select
 #' @importFrom entropy entropy
 #' @importFrom magrittr `%>%`
-#' @param data a data frame or tibble type object
+#' @param data a vector, data frame, or tibble type object
 #' @param ... arguments passed to 'entropy' function
 #' @return a named vector containing the percentage diversity,
 #' the true entropy of the dataset,
@@ -18,18 +18,20 @@
 #' diversity(cars)
 #'
 #' t <- data.frame(a = rep(0, 100))
+#' v <- runif(100)
 #' diversity(t)
+#' diversity(v)
 diversity <- function( data, ... ){
 
   # check assertions ( used in first request )
   d <- list()
   if( is.data.frame( data ) ) {
-    d <- diversity.data.frame( data )
+    d <- diversity_data_frame( data )
   } else if( is.vector( data ) ) {
-    d <- diversity.vector( data )
+    d <- diversity_vector( data )
   }
 
-  return( diversity.plugin( d$fullsize, d$groups ))
+  return( diversity_plugin( d$fullsize, d$groups ))
 }
 
 #' Diversity Vector Helper
@@ -42,7 +44,7 @@ diversity_vector <- function( data ){
   return(
     list(
       'fullsize' = length(data),
-      'groups' = as.data.frame(table(x)) %>%
+      'groups' = as.data.frame(table(data)) %>%
         select('count' = 'Freq') %>%
         unlist()
     )
